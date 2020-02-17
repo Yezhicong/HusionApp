95e1c47a3b31197bcb367375fef8d59b2bb025d7
Yezhicong <15015031796@163.com>
2020-02-17 16:59:10 +0800

    "Delete something unused"

--- app/src/main/java/com/jay/fragmentdemo4/MyFragment1.java
+++ app/src/main/java/com/jay/fragmentdemo4/MyFragment1.java
@@ -2,15 +2,11 @@ package com.jay.fragmentdemo4;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
