f243e49a4a50ab8459e7140b64dc7c80cca78b70
Yezhicong <15015031796@163.com>
2020-02-21 08:50:20 +0800

    "新增自定义扇形按钮控件"

--- app/src/main/java/com/jay/fragmentdemo4/MyFragment4.java
+++ app/src/main/java/com/jay/fragmentdemo4/MyFragment4.java
@@ -7,29 +7,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.jay.fragmentdemo4.MyClass.DLRoundMenuView;
+
+//import com.dlong.rep.dlroundmenuview.DLRoundMenuView;
+//import com.dlong.rep.dlroundmenuview.Interface.OnMenuClickListener;
 
 /**
  * Created by Jay on 2015/8/28 0028.
  */
 public class MyFragment4 extends Fragment {
 
-    private Button logoutBt;
+    //private Button logoutBt;
+    private DLRoundMenuView dlRoundMenuView;
 
     public MyFragment4() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fg_about,container,false);
+        final View view = inflater.inflate(R.layout.fg_camera,container,false);
 
-        logoutBt = view.findViewById(R.id.logout_button);
-        logoutBt.setOnClickListener(new View.OnClickListener() {
+
+        dlRoundMenuView = view.findViewById(R.id.dl_rmv);
+        dlRoundMenuView.setOnMenuClickListener(new DLRoundMenuView.OnMenuClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), LoginActivity.class);
-                startActivity(intent);
+            public void OnMenuClick(int position, int state) {
+                if (state == 1){
+                    Toast.makeText(view.getContext(), "按下：" + position, Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(view.getContext(), "松开：" + position, Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
+        //logoutBt = view.findViewById(R.id.logout_button);
+        //logoutBt.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent = new Intent(v.getContext(), LoginActivity.class);
+        //        startActivity(intent);
+        //   }
+        //});
 
         return view;
     }
